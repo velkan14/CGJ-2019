57bc6ab8bd1d737019623989d293d1e7971e26e7
Zeldan12 <54533904+Zeldan12@users.noreply.github.com>
2019-08-27 11:49:32 +0100

    Movimento

---
+++ Liar/KinematicBody2D.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var isDead = false
+
+var moveDirection = Vector2()
+export var speed = 100
+
+func _physics_process(delta):
+	if !isDead:
+		moveInputs()
+		move()
+
+func moveInputs():
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	var down = Input.is_action_pressed("ui_down")
+	var up = Input.is_action_pressed("ui_up")
+	moveDirection = Vector2(-int(left) + int(right), -int(up) + int (down))
+
+func move():
+	var dir = moveDirection.normalized() * speed
+	move_and_slide(dir, Vector2())
