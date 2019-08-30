fe67f35dcb765f9a0f312d987945492c23195e1c
Daniel Amado <velkan14@gmail.com>
2019-08-30 11:51:06 +0100

    Fixed projectile verification of screen

--- Liar/others/projectiles/Projectile.gd
+++ Liar/others/projectiles/Projectile.gd
@@ -8,23 +8,13 @@ func _ready():
 	set_as_toplevel(true)
 
 func _physics_process(delta):
-	if is_outside_view_bounds():
-		queue_free()
-
-	var motion = direction * SPEED * delta
 	var collision_info = move_and_slide(direction * SPEED)
-	#if collision_info:
-	#	queue_free()
-
-func is_outside_view_bounds():
-	return position.x > OS.get_screen_size().x or position.x < 0.0 \
-		or position.y > OS.get_screen_size().y or position.y < 0.0
-
-func _draw():
-	draw_circle(Vector2(), $CollisionShape2D.shape.radius, Color('#ffffff'))
-
 
 func _on_HitBox_body_entered(body):
 	if body.is_in_group("enemy"):
 		#body.hit(0.5)
 		queue_free()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	print("EXIT")
+	queue_free()
