d2b887e6c330dc3972a34483623543676318a3c6
Daniel Amado <velkan14@gmail.com>
2019-08-31 14:58:24 +0100

    Mape with tilemap

--- Liar/others/PlateTrigger.gd
+++ Liar/others/PlateTrigger.gd
@@ -2,5 +2,15 @@ extends Area2D
 
 signal pressed
 
+onready var sprite = $Sprite
+
 func _on_PlateTrigger_body_entered(body):
-	emit_signal("pressed")
+	if body.is_in_group("player"):
+		emit_signal("pressed")
+		sprite.frame = 6
+
+func _on_PlateTrigger_body_exited(body):
+	if body.is_in_group("player"):
+		emit_signal("unpressed")
+		sprite.frame = 5
+		
